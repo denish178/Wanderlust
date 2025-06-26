9610799ae25775f97f6d156e4eba89be4e2871a1
Denish Murawala <murawaladenish@gmail.com>
2025-06-26 11:35:46 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 import express from "express";
+import mongoose from "mongoose";
+import Listing from "./models/listing.js";
 
 const app = express();
-const mongoose = re;
+const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
+
+async function main() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Connected to DB");
+
+    // Start server only after DB connects
+    app.listen(8080, () => {
+      console.log("server is running!");
+    });
+  } catch (err) {
+    console.log("MongoDB connection error:", err);
+  }
+}
+
+main();
+
+app.get("/", (req, res) => {
+  res.send("I AM ROOT!");
+});
+
+app.get("/testListing", async (req, res) => {
+  const sampleListing = new Listing({
+    title: "My Home",
+    description: "Near Rameshvaram temple",
+    image: "",
+    price: 1200,
+    location: "kerela, Rameshvaram",
+    country: "india",
+  });
+
+  await sampleListing.save();
+  console.log("SAMPLE WAS SAVED");
+  res.send("SUCCESS");
+});
---
+++ models/listing.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1739866669727-8d75caed31a0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1739866669727-8d75caed31a0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  //v = value here
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
